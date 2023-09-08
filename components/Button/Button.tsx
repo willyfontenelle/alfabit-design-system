1cf8c1725acfc8409adc2257326f794f6fd0ddcf
Willy Fontenelle <willyfontenelle@gmail.com>
2023-09-08 04:10:22 -0300

    fim-do-curso-1

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,20 +1,29 @@
 export type ButtonProps = {
   children: React.ReactNode;
+  variant?: 'primary' | 'secondary' | 'tertiary';
 } & React.ButtonHTMLAttributes<HTMLButtonElement>;
 
-const Button = ({ children, className, disabled, ...rest }: ButtonProps) => {
-  const Btn = (classes: string) => {
-    //const generalStyle = 'rounded-[8px] px-[32px] py-[12px]'
-    const generalStyle = 'rounded-md px-6 py-2'
+function getVariant(variant: ButtonProps['variant'], disabled: ButtonProps['disabled']) {
+  switch (variant) {
+    case 'primary':
+      return disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white'
+  case 'secondary':
+    return disabled ? 'bg-disabled text-disabled' : 'bg-quaternary text-primary'
+    case 'tertiary':
+  default:
+    return disabled ? 'text-disabled' : 'text-primary'
+}
+}
+
+
+const Button = ({ variant = 'primary', children, className, disabled, ...rest }: ButtonProps) => {
     return <button
-      className={`${generalStyle} ${classes} ${className}`}
+      className={`rounded-md px-6 py-2 ${getVariant(variant, disabled)} ${className}`}
       disabled={disabled}
       {...rest}
     >
-      { children }
+      {children}
     </button>
-  }
-  return Btn(disabled ? 'bg-disabled text-disabled' : 'bg-primary text-white')
 
 }
 
